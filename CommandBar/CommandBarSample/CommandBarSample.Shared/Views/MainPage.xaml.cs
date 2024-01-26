0d8afe20f40060719767d7d9d3dd785f1ff237ae
G Key <g.key0325@gmail.com>
2024-01-26 01:15:07 -0800

    first

---
+++ CommandBar/CommandBarSample/CommandBarSample.Shared/Views/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace CommandBarSample.Shared.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        Type currentPage;
+        public MainPage()
+        {
+            InitializeComponent();
+            currentPage = typeof(HomePage);
+            contentView.Navigate(currentPage);
+        }
+
+        void OnAppBarButtonTapped(object sender, RoutedEventArgs args)
+        {
+            if (sender is FrameworkElement element)
+            {
+                var tag = element.Tag.ToString();
+                Type page = null;
+                switch (tag)
+                {
+                    case "Home":
+                        page = typeof(HomePage);
+                        break;
+                    case "Info":
+                        page = typeof(InfoPage);
+                        break;
+                    case "About":
+                        page = typeof(AboutPage);
+                        break;
+                }
+
+                if (currentPage != page)
+                {
+                    currentPage = page;
+                    contentView.Navigate(page);
+                }
+            }
+        }
+    }
+}
---
+++ Navigation/src/Uno.Samples.Navigation.Shared/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Uno.Samples.Navigation
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, RoutedEventArgs args)
+        {
+            if (sender == Basic)
+                Frame.Navigate(typeof(BasicNavigation.MainPage));
+            else if (sender == Argument)
+                Frame.Navigate(typeof(ArgumentNavigation.MainPage));
+            else if (sender == Animation)
+                Frame.Navigate(typeof(AnimationNavigation.MainPage));
+        }
+    }
+}
